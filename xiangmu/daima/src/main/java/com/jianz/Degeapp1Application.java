af7bc5267b960e77d734379218648aa5eb7da58e
Administrator <Administrator@I91T1FM30VWNVX5>
2019-08-16 19:14:05 +0800

    马德旺

---
+++ xiangmu/daima/src/main/java/com/jianz/Degeapp1Application.java
@@ -0,0 +1,13 @@
+package com.jianz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**测试类*/
+@SpringBootApplication
+public class Degeapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Degeapp1Application.class, args);
+	}
+
+}
